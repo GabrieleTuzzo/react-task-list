449c51bc660b24c4096b874794cb3a8e5bca3c65
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-26 17:23:00 +0100

    ADD logic solution with no formatting or styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import './App.css';
+import { tasks } from './assets/tasks';
 
 function App() {
+    const completedArray = tasks.filter((task) => task.state === 'completed');
+    const notCompletedArray = tasks.filter(
+        (task) => task.state !== 'completed'
+    );
+
     return (
         <>
-            <div>Hello World</div>
+            <h1>Task Manager</h1>
+            <h2>Current Tasks ({notCompletedArray.length})</h2>
+            <ul>
+                {notCompletedArray.map((task) => (
+                    <li key={task.id}>
+                        <strong>{task.title}</strong>
+                        <span>{task.state}</span>
+                        <p>{task.priority}</p>
+                        <p>{task.estimatedTime}</p>
+                    </li>
+                ))}
+            </ul>
+            <hr></hr>
+            <h2>Completed Tasks ({completedArray.length})</h2>
+            <ul>
+                {completedArray.map((task) => (
+                    <li key={task.id}>
+                        <strong>{task.title}</strong>
+                        <span>{task.state}</span>
+                        <p>{task.priority}</p>
+                        <p>{task.estimatedTime}</p>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
