1c4e2e2ec111238704a5e91a08f8660038cc14d4
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-26 16:26:45 +0100

    REMOVE unused files and cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
     return (
         <>
-            <div>
-                Hello <World></World>
-            </div>
+            <div>Hello World</div>
         </>
     );
 }
