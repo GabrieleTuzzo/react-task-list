8a207a61a793ba7bf350aaa432ae9c8a9caa9921
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-26 16:21:51 +0100

    ADD basic project scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div>
+                Hello <World></World>
+            </div>
+        </>
+    );
+}
+
+export default App;
